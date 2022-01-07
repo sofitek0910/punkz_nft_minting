99d093ba3f9c33026e9bdeaa1f7060f536932b20
sunshine09101 <sunshine09101@gmail.com>
2022-01-07 01:23:58 +0100

    main structure done

--- src/App.js
+++ src/App.js
@@ -136,6 +136,9 @@ function App() {
                 <a href="#">
                     <img className="top-icon" src="./assets/images/twitter.png"/>
                 </a>
+				<a href="#">
+                    <img className="top-icon" src="./assets/images/video.png"/>
+                </a>
                 <a href="#">
                     <img className="top-icon" src="./assets/images/group.png"/>
                 </a>
@@ -143,10 +146,10 @@ function App() {
         </div>
         
         <header className="navbar-block">
-            <nav>
-                {(toggleMenu || screenWidth > 768) && (
+            <nav> 
+				<img className="logo-image" src="./assets/images/logo.png"/>
+                {(toggleMenu || screenWidth > 1060) && (
                     <div className="menu-block">
-                        <img className="logo-image" src="./assets/images/logo.png"/>
                         <ul className="list">
                             <li className="items">Buy</li>
                             <li className="items">Roadmap</li>
@@ -165,8 +168,8 @@ function App() {
         </div>
         <section className="welcome-section content-section">
             <div className="row d-flex flex-center">
-                <div className="col-sm-8">
-                    <div className="title-font">
+                <div className="col-md-8 col-sm-12">
+                    <div className="title-font welcome-font">
                         WELCOME TO THE DEAD PUNKZ
                     </div>
                     <div className="content-font1">
@@ -178,7 +181,7 @@ function App() {
                         will blow every other out of the water.
                     </div>
                 </div>
-                <div className="col-sm-4">
+                <div className="col-md-4 col-sm-12">
                     <img className="width-100" src="./assets/images/cat.gif"/>
                 </div>
             </div>
@@ -187,7 +190,7 @@ function App() {
         <section className="buy-section content-section">
             <div className="title-font">BUY</div>
             <div className="row d-flex flex-center">
-                <div className="col-sm-8">
+                <div className="col-md-8 col-sm-12">
                     <div className="content-font1">
                         Every SPOOKY SKELLY PUNK costs .1 eth.There will
                         be a presale for whitelist members, and a public
@@ -196,8 +199,8 @@ function App() {
                         & rewards. 
                     </div>
                 </div>
-                <div className="col-sm-4 text-center">
-                    <button className="btn mint-btn">MINT</button>
+                <div className="col-md-4 col-sm-12 text-center">
+                    <button className="mint-btn rubik-font">MINT</button>
                 </div>
             </div>
         </section>
@@ -205,8 +208,8 @@ function App() {
         <section className="spec-section content-section">
             <div className="title-font">TEH SPECS</div>
             <div className="row d-flex flex-center">
-                <div className="col-sm-9">
-                    <div className="content-font2">
+                <div className="col-md-9 col-sm-12">
+                    <div className="content-font2 specs-content-text">
                         Each SPOOKY SKELLY PUNK is unique and programmatically generated from over 200 possible traits, including headwear, mouth pieces, glasses, 
                         and more. All SPOOKY SKELLYS have abilities and traits that make some rarer than others. Every Spooky Skelly is unique and sick. 
                         These DEAD PUNKZ are stored as ERC-721 tokens on the Ethereum blockchain. Check out how rare your skelly is through
@@ -214,18 +217,25 @@ function App() {
                         You must be signed into you rmetamask wallet to enter the members club.
                     </div>
                 </div>
-                <div className="col-sm-3 text-center">
-                    <button className="btn specs-btn rarity-btn">RARITY.TOOLS</button>
-                    <button className="btn specs-btn club-btn">
-                        ENTER MEMBERS<br/>
-                        ONLY CLUB
-                    </button>
+                <div className="col-md-3 col-sm-12 text-center">
+					<div className="row">
+						<div className="col-md-12 col-sm-6">
+							<a href="#">
+								<img className="width-100 club-btn" src="./assets/images/rarity.png"></img>
+							</a>
+						</div>
+						<div className="col-md-12 col-sm-6">
+							<a href="#">
+								<img className="width-100 club-btn" src="./assets/images/club.png"></img>
+							</a>
+						</div>
+					</div>
                 </div>
             </div>
         </section>
 
         <section className="roadmap-section content-section">
-            <div className="title-font">TEH ROADMAP</div>
+            <div className="title-font">ROADMAP</div>
             <div className="content-font2">
                 Our Roadmap catalogs a handful of future utilities and benefits we wish to bring 
                 our members as our community develops. Glance over the phases to see a few of the 
@@ -234,131 +244,132 @@ function App() {
                 sure we provide the top meta verse experience
             </div>
             <div className="row d-flex roadmap-block">
-                <div className="col-sm-6 text-center">
+                <div className="col-md-6 col-sm-12 text-center">
                     <img className="width-100" src="./assets/images/Roadmap_image.png"/>
                 </div>
-                <div className="col-sm-6">
+                <div className="col-md-6 col-sm-12">
 					<div className="accordion" id="accordionExample">
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="headingOne">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
 									<button className="phase-btn">phase 1</button> initiation
 								</div>
 							</h2>
 							<div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+								<div className="accordion-body rubik-font">
 									This is roadmap answer for intiation one
 								</div>
 							</div>
 						</div>
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="headingTwo">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
 									<button className="phase-btn">phase 2</button> reveal
 								</div>
 							</h2>
 							<div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 2.
 								</div>
 							</div>
 						</div>
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="heading3">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
 									<button className="phase-btn">phase 3</button> vamp airdrops
 								</div>
 							</h2>
-							<div id="collapse3" className="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+							<div id="collapse3" className="accordion-collapse collapse rubik-font" aria-labelledby="heading3" data-bs-parent="#accordionExample">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 3.
 								</div>
 							</div>
 						</div>
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="heading4">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
 									<button className="phase-btn">phase 4</button> DEAD PUNKZ Z
 								</div>
 							</h2>
-							<div id="collapse4" className="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+							<div id="collapse4" className="accordion-collapse collapse rubik-font" aria-labelledby="heading4" data-bs-parent="#accordionExample">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 4.
 								</div>
 							</div>
 						</div>
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="heading3">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+								<div className="accordion-button collapsed 
+								rubik-font" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
 									<button className="phase-btn">phase 5</button> mummy airdrops
 								</div>
 							</h2>
 							<div id="collapse5" className="accordion-collapse collapse" aria-labelledby="heading5" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 5.
 								</div>
 							</div>
 						</div>
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="heading6">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
 									<button className="phase-btn">phase 6</button> merch realease
 								</div>
 							</h2>
 							<div id="collapse6" className="accordion-collapse collapse" aria-labelledby="heading6" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 6.
 								</div>
 							</div>
 						</div>
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="heading7">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse7" aria-expanded="false" aria-controls="collapse7">
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapse7" aria-expanded="false" aria-controls="collapse7">
 									<button className="phase-btn">phase 7</button> customization
 								</div>
 							</h2>
 							<div id="collapse7" className="accordion-collapse collapse" aria-labelledby="heading7" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 7.
 								</div>
 							</div>
 						</div>
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="heading8">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse8" aria-expanded="false" aria-controls="collapse8">
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapse8" aria-expanded="false" aria-controls="collapse8">
 									<button className="phase-btn">phase 8</button> dead punkz festival
 								</div>
 							</h2>
 							<div id="collapse8" className="accordion-collapse collapse" aria-labelledby="heading8" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 8.
 								</div>
 							</div>
 						</div>
-						{/* <div className="accordion-item">
+						<div className="accordion-item">
 							<h2 className="accordion-header" id="heading9">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse9" aria-expanded="false" aria-controls="collapse9">
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapse9" aria-expanded="false" aria-controls="collapse9">
 									<button className="phase-btn">phase 9</button> deadverse beta
 								</div>
 							</h2>
 							<div id="collapse9" className="accordion-collapse collapse" aria-labelledby="heading9" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 9.
 								</div>
 							</div>
 						</div>
 						<div className="accordion-item">
 							<h2 className="accordion-header" id="heading10">
-								<div className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse10" aria-expanded="false" aria-controls="collapse10">
-									<button className="phase-btn">phase 10</button> deadverse
+								<div className="accordion-button collapsed rubik-font" data-bs-toggle="collapse" data-bs-target="#collapse10" aria-expanded="false" aria-controls="collapse10">
+									<button className="phase-btn phase10">phase 10</button> deadverse
 								</div>
 							</h2>
 							<div id="collapse8" className="accordion-collapse collapse" aria-labelledby="heading10" data-bs-parent="#accordionExample">
-								<div className="accordion-body">
+								<div className="accordion-body rubik-font">
 									This is the answer for phase 10.
 								</div>
 							</div>
-						</div> */}
+						</div>
 					</div>
                 </div>
             </div>
@@ -401,32 +412,54 @@ function App() {
 				and ask any questions about DEAD PUNKZ NFTS.
 			</div>
 			<div className="join-discord text-center">
-				<button className="discord-button">
+				<button className="discord-button rubik-font">
 					JOIN DISCORD
 				</button>
 			</div>
-			<div className="smart-contract">
+			<div className="smart-contract rubik-font">
 				VERIFIED SMART CONTRACT ADDRESS:
 			</div>
         </section>
 
-		<section className="community-section content-section">
+		<section className="bottom-section">
 			<div className="row d-flex flex-center">
-                <div className="col-sm-4">
+                <div className="col-md-4 col-sm-12">
 					<div className="subscribe">
-						<div class="mx-5">
+						<div className="subscribe-title">GET ON THE LIST</div>
+						<div class="mx-0">
 							<div class="input-group mb-3 mt-4"> 
 								<input type="text" class="form-control" placeholder="EMAIL ADDRESS" aria-label="Recipient's username" aria-describedby="button-addon2"/> 
-								<button class="btn btn-success border-rad" type="button" id="button-addon2">-&#10095;</button> 
+								<button class="btn btn-success border-rad" type="button" id="button-addon2">&#10132;</button> 
 							</div>
 						</div>
 					</div>
                 </div>
-				<div className="col-sm-4">
+				<div className="col-md-4 col-sm-12">
 					<img className="width-100" src="./assets/images/logo.png"/>
                 </div>
-				<div className="col-sm-4">
-					{/* <img className="width-100 team-image" src="./assets/images/member3.png"/> */}
+				<div className="col-md-4 col-sm-12">
+					<div className="bottom-icon-section">
+						<div className="bottom-icon-block">
+							<a href="#">
+								<img className="top-icon" src="./assets/images/discord.png"/>
+							</a>
+							<a href="#">
+								<img className="top-icon" src="./assets/images/instagram.png"/>
+							</a>
+							<a href="#">
+								<img className="top-icon" src="./assets/images/twitter.png"/>
+							</a>
+							<a href="#">
+								<img className="top-icon" src="./assets/images/video.png"/>
+							</a>
+						</div>
+					</div>
+					<div className="bottom-text-block">
+						<div>&#169; 2022 DEAD PUNKZ LLC</div>
+						<div>DPTERMS # CONDITIONS</div>
+						<div>SSPTERMS # CONDITIONS</div>
+						<div>DPZTERMS # CONDITIONS</div>
+					</div>
                 </div>
             </div>
         </section>
