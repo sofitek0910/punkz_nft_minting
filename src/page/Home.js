e9c3ffade5a8d89adec2e44c80779be96ffede2f
sunshine09101 <sunshine09101@gmail.com>
2022-01-14 22:40:14 +0100

    code clean

--- src/page/Home.js
+++ src/page/Home.js
@@ -125,7 +125,7 @@ function App() {
     setSpecsHeight($(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height()+$(".welcome-section").height()+$(".buy-section").height()-50);
     setRoadmapHeight($(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height()+$(".welcome-section").height()+$(".buy-section").height()+$(".spec-section").height());
     setCommunityHeight($(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height()+$(".welcome-section").height()+$(".buy-section").height()+$(".spec-section").height()+$(".roadmap-section").height()+$(".team-section").height()+250);
-    console.log("scroll------------->", window.scrollY)
+    console.log("scroll--->", window.scrollY)
     console.log('scroll event', $(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height());
   }
   
@@ -524,7 +524,6 @@ function App() {
                 </div>
             </div>
         </section>
-
     </div>
   );
 }
